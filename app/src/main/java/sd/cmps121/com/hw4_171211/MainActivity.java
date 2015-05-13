8af586dc179f729a0292ea556895d91a092446b9
Shivam Dave <sdave@ucsc.edu>
2015-05-13 14:33:20 -0700

    Added 2 activities to manifest/xmls/classes.
    MainActivity will be where we navigate to a newspaper/news site
    ReaderActivity will display the webview/site
    
    Also gave internet permissions to the app

---
+++ app/src/main/java/sd/cmps121/com/hw4_171211/MainActivity.java
@@ -0,0 +1,22 @@
+package sd.cmps121.com.hw4_171211;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by shivamndave on 5/13/15.
+ */
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/sd/cmps121/com/hw4_171211/ReaderActivity.java
@@ -0,0 +1,9 @@
+package sd.cmps121.com.hw4_171211;
+
+import android.app.Activity;
+
+/**
+ * Created by shivamndave on 5/13/15.
+ */
+public class ReaderActivity extends Activity {
+}
